50a503370ec99c49b39553cbfa7a129fe52551fc
pmyth6 <pmyth6@nottingham.ac.uk>
2024-10-19 15:38:03 +0100

    Changing the colours of the blocks

--- tetris-ui/colors.py
+++ tetris-ui/colors.py
@@ -8,15 +8,16 @@ Created on Fri Oct 18 11:18:53 2024
 
 class Colors:
     dark_grey = (26, 31, 40)
-    green = (47, 230, 23)
-    red = (232, 18, 18)
     orange = (226, 116, 17)
-    yellow = (237, 234, 4)
-    purple = (166, 0, 247)
-    cyan = (21, 204, 209)
     blue = (13, 64, 216)
+    cyan = (21, 204, 209)
+    yellow = (237, 234, 4)
+    green = (47, 230, 23)
+    purple = (166, 0, 247)
+    red = (232, 18, 18)
+
     
     @classmethod 
     def get_cell_colors(cls):
-        return [cls.dark_grey, cls.green, cls.red, cls.orange, cls.yellow,
-                cls.purple, cls.cyan, cls.blue]
+        return [cls.dark_grey, cls.orange, cls.blue, cls.cyan, cls.yellow,
+                cls.green, cls.purple, cls.red]
